e4242280153f4cba284214f239c5ff6eb3110631
Shaurya Malik <shauryamaliik@gmail.com>
2023-08-11 03:16:15 +0530

    init

--- src/components/homepage/main-homepage.js
+++ src/components/homepage/main-homepage.js
@@ -1,12 +1,14 @@
 import NavbarMain from "./NavbarMain";
 import Banner from './banner';
 import ContactUs from './contact';
+import Content from "./content";
 
 const MainHomepage = () => {
   return (
     <>
         <NavbarMain />
         <Banner />
+        <Content />
         <ContactUs />
     </>
   );
